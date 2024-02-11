2fb8f3292988966e991d0d0904ef72f97dd672c7
jammisandy <sandeepjammi@gmail.com>
2024-02-11 11:07:26 -0600

    Push

---
+++ Day-13/Debug1.py
@@ -0,0 +1,6 @@
+def my_function():
+    for i in range(1,21):
+        if i == 20:
+            print("Hello Sandy!")
+
+my_function()
---
+++ Day-13/Debug2.py
@@ -0,0 +1,5 @@
+# Reproduce the Bug
+from random import randint
+dice_imgs = ["❶", "12", "34", "9", "56", "29"]
+dice_num = randint(0, 5)
+print(dice_imgs[dice_num])
