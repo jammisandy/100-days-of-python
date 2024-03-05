dc11d6133afd4e09dfd32106a1e3f28033f7f839
jammisandy <sandeepjammi@gmail.com>
2024-03-05 11:49:49 -0600

    push

--- Day-23/player.py
+++ Day-23/player.py
@@ -10,9 +10,19 @@ class Player(Turtle):
         self.shape("turtle")
         self.penup()
         self.color("Blue")
-        self.goto(STARTING_POSITION)
+        self.go_to_start()
         self.setheading(90)
 
 
     def go_up(self):
         self.forward(MOVE_DISTANCE)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
