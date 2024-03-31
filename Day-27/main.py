1bb1de2b963643b82adc15b821425e0f9f5f72ac
jammisandy <sandeepjammi@gmail.com>
2024-03-30 20:03:50 -0500

    Done

--- Day-27/main.py
+++ Day-27/main.py
@@ -1,13 +1,4 @@
-import tkinter
- 
-window = tkinter.Tk()
-window.title("My First GUI program")
-window.minsize(width=500, height=300)
-my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack()
-
-my_label["text"] = "New Text"
-my_label.config(text="New Text")
+import tkinter as tk
 
 
 def button_clicked():
@@ -15,19 +6,29 @@ def button_clicked():
     new_text = input.get()
     my_label.config(text=new_text)
 
-button = tkinter.Button(text="Click Me", command=button_clicked)
-button.pack()
 
-##Entry
+window = tk.Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+window.config(padx=100, pady=200)
 
-input = tkinter.Entry(width=10)
-input.pack()
-input.get()
-# import turtle
+#Label
+my_label = tk.Label(text="I Am a Label", font=("Arial", 24, "bold"))
+my_label.config(text="New Text")
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
 
-# tim = turtle.Turtle()
-# tim.write()
+#Button
+button = tk.Button(text="Click Me", command=button_clicked)
+button.grid(column=1, row=1)
 
+new_button = tk.Button(text="New Button")
+new_button.grid(column=2, row=0)
+
+#Entry
+input = tk.Entry(width=10)
+print(input.get())
+input.grid(column=3, row=2)
 
 
 
